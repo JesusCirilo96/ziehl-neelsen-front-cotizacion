5fe6e0669eaad1af357c7ccd018ffba405beffed
Jesus Cirilo <cirlo21@outlook.es>
2020-09-03 17:54:13 -0500

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { from } from 'rxjs';
+
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {CotizacionComponent} from './components/cotizacion/cotizacion.component';
+
+const routes: Routes = [
+  {path: '', component: WelcomeComponent },
+  {path: 'cotizar', component: CotizacionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//components
+import { CotizacionComponent } from './components/cotizacion/cotizacion.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CotizacionComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
